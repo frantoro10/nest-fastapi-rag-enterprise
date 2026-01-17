94313994f83ab5dd1cb475cdab8bec8672391734
Francisco Toro <torofrancisco13@gmail.com>
2026-01-16 22:34:07 -0300

    chore: database config

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configuration module - Load .env file (Carga archivo env, modulo de configuracion)
+    ConfigModule.forRoot({
+      isGlobal: true, // To use the variables everywhere
+    }),
+
+    // Database module
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true, // Carga automaticamente las entidades - Load entities automatically
+        synchronize: false, // En producción es false. - In production is false
+        ssl: {
+          rejectUnauthorized: false, // For safe conection to Supabase
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
