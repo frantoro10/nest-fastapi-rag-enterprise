1bc02c59b289496511adb2d2c0885cbae05877e5
Francisco Toro <torofrancisco13@gmail.com>
2026-01-22 05:03:25 -0300

    chore: entity module

--- gateway/src/documents/documents.module.ts
+++ gateway/src/documents/documents.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Document } from './entities/document.entity';
 import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
 
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Document])],
   controllers: [DocumentsController],
   providers: [DocumentsService],
 })
