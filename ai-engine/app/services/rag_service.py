bd31a8f31f9230a62a66267819d2899bc1594fc3
Francisco Toro <torofrancisco13@gmail.com>
2026-01-30 05:37:23 -0300

    feat(ragservice) modules imported

--- ai-engine/app/services/rag_service.py
+++ ai-engine/app/services/rag_service.py
@@ -1,4 +1,19 @@
+import os
 import logging
+from pathlib import Path
+from typing import List, Optional
+
+# External Clients 
+from supabase import create_client, Client
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+# Internal lModules
 from app.models.rag_schemas import FileProcessingRequest
 
 logger = logging.getLogger(__name__)
@@ -13,5 +28,5 @@ class RAGService:
         3. Generate embeds (OpenAI)
         4. Save to Vector Store (Supabase pgvector)
         """
-        
-        
+
+
