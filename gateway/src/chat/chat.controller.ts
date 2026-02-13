2d75b09e0d5946f2de5e39e255a77d36233b873e
Francisco Toro <torofrancisco13@gmail.com>
2026-02-13 06:11:31 -0300

    feat: chat controller done

--- gateway/src/chat/chat.controller.ts
+++ gateway/src/chat/chat.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
+import { AskQuestionDto } from './dto/ask-question.dto';
+import { ChatService } from './chat.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('chat')
-export class ChatController {}
+@UseGuards(JwtAuthGuard)// Secures the endpoint. Requires Bearer Token.
+export class ChatController {
+    constructor(private readonly chatService: ChatService){}
+
+    @Post('ask')
+    async askQuestion(@Request() req, @Body() askQuestionDto: AskQuestionDto){
+        // req.user is automatically populated by the JwtAuthGUard
+        const userId = req.user.sub;
+        const { question } = askQuestionDto;
+
+        const answer = await this.chatService.askQuestion(userId, question);
+
+        return {
+            success: true,
+            answer: answer,
+        };
+    }
+}
